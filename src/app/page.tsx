fab4b2ca76585c669288d9410c9b3918b063ae9c
azanniel <leo.azannielttt@gmail.com>
2023-08-02 15:43:02 -0400

    ✨ add sidebar component with logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex h-screen w-full items-center justify-center">
+    <div className="">
       <h1>Hello Tailwind</h1>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx'
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+      },
+    },
   },
   plugins: [],
 }
