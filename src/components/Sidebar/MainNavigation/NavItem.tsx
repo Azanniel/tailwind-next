7eba08409ef7ab02cf0725d907f4c7e097d06907
azanniel <leo.azannielttt@gmail.com>
2023-08-02 16:02:58 -0400

    ✨ add main navigation

---
+++ src/components/Sidebar/MainNavigation/NavItem.tsx
@@ -0,0 +1,22 @@
+import { ElementType } from 'react'
+import { ChevronDown } from 'lucide-react'
+
+interface NavItemProps {
+  title: string
+  icon: ElementType
+}
+
+export function NavItem({ title, icon: Icon }: NavItemProps) {
+  return (
+    <a
+      className="group flex items-center gap-3 rounded px-3 py-2 hover:bg-violet-50"
+      href=""
+    >
+      <Icon className="h-5 w-5 text-zinc-500" />
+      <span className="font-medium text-zinc-700 group-hover:text-violet-500">
+        {title}
+      </span>
+      <ChevronDown className="ml-auto h-5 w-5 text-zinc-400 group-hover:text-violet-400" />
+    </a>
+  )
+}
