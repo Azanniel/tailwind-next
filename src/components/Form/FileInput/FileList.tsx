4c251a6c4cd8479308797eaa1460e2e55f41d098
azanniel <leo.azannielttt@gmail.com>
2023-09-20 23:23:09 -0400

    ♻️ change button element to button ghost variant

--- src/components/Form/FileInput/FileList.tsx
+++ src/components/Form/FileInput/FileList.tsx
@@ -6,6 +6,7 @@ import { useAutoAnimate } from '@formkit/auto-animate/react'
 import { useFileInput } from './Root'
 
 import { formatBytes } from '@/utils/format-bytes'
+import { Button } from '@/components/Button'
 
 export function FileList() {
   const { files } = useFileInput()
@@ -43,12 +44,9 @@ export function FileList() {
               </div>
             </div>
 
-            <button
-              type="button"
-              className="ml-auto rounded-md p-2 hover:bg-zinc-50"
-            >
+            <Button variant="ghost" type="button">
               <Trash2 className="h-5 w-5 text-zinc-500" />
-            </button>
+            </Button>
           </div>
         )
       })}
--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -1,4 +1,5 @@
 import { LogOut } from 'lucide-react'
+import { Button } from '../Button'
 
 export function Profile() {
   return (
@@ -18,9 +19,9 @@ export function Profile() {
         </span>
       </div>
 
-      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+      <Button variant="ghost" type="button">
         <LogOut className="h-5 w-5 text-zinc-500" />
-      </button>
+      </Button>
     </div>
   )
 }
